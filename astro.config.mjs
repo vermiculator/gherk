166ca6b10a15e3458664ad2ca87974c7e7949c32
gherk <rowanlucas@protonmail.com>
2025-03-22 15:37:09 +0000

    add remark-wiki-links with patch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-
 import react from '@astrojs/react';
+import wikiLinkPlugin from "@portaljs/remark-wiki-link";
+import { getPermalinks } from "@portaljs/remark-wiki-link";
 
+const permalinks = await getPermalinks("./src/content/");
 // https://astro.build/config
 export default defineConfig({
+    markdown: {
+        remarkPlugins: [
+          [wikiLinkPlugin, { 
+            pathFormat: 'obsidian-short', 
+            permalinks,
+            // generate url of the linked page.
+            // here `slug` would be "Page Name" for wiki link [[Page Name]].
+            wikiLinkResolver: (/** @type {any} */ slug) => [slug] 
+          }],
+        ]
+    },
     integrations: [starlight({
         title: 'gherk',
         social: {
