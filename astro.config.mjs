9ea0f83e1949daca55651fba2ba08b6e5584a701
gherk <rowanlucas@protonmail.com>
2025-05-07 22:39:10 +0100

    working structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,41 +2,87 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import react from '@astrojs/react';
-import wikiLinkPlugin from "@portaljs/remark-wiki-link";
-import { getPermalinks } from "@portaljs/remark-wiki-link";
+import starlightObsidian, { obsidianSidebarGroup } from 'starlight-obsidian';
+import starlightSiteGraph from 'starlight-site-graph';
 
-const permalinks = await getPermalinks("./src/content/");
 // https://astro.build/config
 export default defineConfig({
-    markdown: {
-        remarkPlugins: [
-          [wikiLinkPlugin, { 
-            pathFormat: 'obsidian-short', 
-            permalinks,
-            // generate url of the linked page.
-            // here `slug` would be "Page Name" for wiki link [[Page Name]].
-            wikiLinkResolver: (/** @type {any} */ slug) => [slug] 
-          }],
-        ]
-    },
     integrations: [starlight({
         title: 'gherk',
-        social: {
-            github: 'https://github.com/rowanlucas',
-        },
+        social: [
+            { icon: 'github', label: 'GitHub', href: 'https://github.com/rowanlucas' },
+        ],
         sidebar: [
             {
                 label: 'earth',
+                collapsed: true,
                 autogenerate: { directory: 'earth' },
             },
             {
                 label: 'library',
+                collapsed: true,
                 autogenerate: { directory: 'library' },
             },
             {
-                label: 'swamp',
-                autogenerate: { directory: 'swamp' },
+                label: 'vignettes',
+                link: '/vignettes'
             },
         ],
+        plugins: [
+            starlightObsidian({
+              vault: './src/content/vault',
+              ignore: ['./*.md', 'id.md','.filenignore','.megaignore','*.acsm','LICENSE','*.txt','*.pdf','desktop.ini','./Rubbish','_.debris','.trash','./seeds', './private', './assets', './themes', './vignettes'],
+              output: '',
+              autoLinkHeadings: true,
+              copyFrontmatter: 'all',
+              tableOfContentsOverview: 'title'
+            }),
+            starlightSiteGraph({
+                debug: false,
+                trackVisitedPages: 'disable',
+                sitemapConfig: {
+                    includeExternalLinks: true,
+                    ignoreStarlightLinks: true,
+                    linkInclusionRules: ["!https://read.readwise.io/read/*", "**/*"],
+                },
+                backlinksConfig: {},
+                graphConfig: {
+                    "depth": 2,
+                    "depthDirection": "both",
+                    "actions": [
+                        "fullscreen",
+                        "depth"
+                    ],
+                    "repelForce": 333,
+                    "colliderPadding": 18,
+                    "alphaDecay": 0.023,
+                    "enableDrag": true,
+                    "enablePan": true,
+                    "enableZoom": true,
+                    "enableHover": true,
+                    "enableClick": "click",
+                    "prefetchPages": false,
+                    "renderUnresolved": true,
+                    "renderArrows": true,
+                    "renderExternal": true,
+                    "nodeDefaultStyle": {
+                        "shape": "circle",
+                        "shapeSize": 4,
+                        "shapeColor": "backgroundColor",
+                        "strokeColor": "nodeColor",
+                        "strokeWidth": 2,
+                        "cornerType": "bevel",
+                        "neighborScale": 0.4,
+                      },
+                    "linkWidth": 2,
+                    "linkHoverWidth": 3,
+                    "visibilityRules": [
+                        "**/*"
+                      ],
+
+                },
+            }),
+          ]
+          //.concat(process.env.CHECK_LINKS ? starlightLinksValidator() : []),
 		}), react()],
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -3,24 +3,53 @@ import { glob } from 'astro/loaders';
 
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
+import { pageSiteGraphSchema } from 'starlight-site-graph/schema';
 
 export const collections = {
-	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
-	earth: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
-	library: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
-	siteMeta: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
-	swamp: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+	docs: defineCollection({ 
+		loader: docsLoader(), 
+		schema: docsSchema({
+			extend: pageSiteGraphSchema
+		}),
+	}),
+	earth: defineCollection({ 
+		loader: docsLoader(), 
+		schema: docsSchema({
+			extend: z.object({
+			  // Make a built-in field required instead of optional.
+			  // description: z.string(),
 
+			  // Add a new field to the schema.
+			  garden: z.enum(['soil', 'seed', 'seedling', 'sprout', 'green']).optional(),
+			  parent: z.array(reference('earth')).optional(),
+			  peer: z.array(reference('earth')).optional(),
+			  child: z.array(reference('earth')).optional(),
+			}).merge(pageSiteGraphSchema),
+		  }), 
+	}),
+	library: defineCollection({ 
+		loader: docsLoader(), 
+		schema: docsSchema({
+			extend: z.object({
+			  status: z.enum(['DORMANT', 'CURRENTLY', 'ARCHIVED']).optional(),
+			  author: z.array(z.string()).optional(),
+			  topics: z.array(reference('docs')).optional(),
+			}).merge(pageSiteGraphSchema),
+		  }), 
+	}),
+	siteMeta: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
 	vignettes: defineCollection({ 
 		loader: glob({ pattern: ['**/*.md'], base: "./src/content/vignettes/data" }),
 		schema: z.object({
 			hide: z.boolean().optional(),
+			format: z.enum(['image', 'text']),
 			kind: reference('vignetteTypes'),
 			image: z.object({
 				src: z.string(),
 				alt: z.string().optional(),
-			}),
-			text: z.string().optional(),
+			}).optional(),
+			title: z.string().optional(),
+			caption: z.string().optional(),
 			date: z.date().optional(), // YYYY-MM-DD
 		}),
 	}),
@@ -35,7 +64,16 @@ export const collections = {
 			})
 		),
 	}),
-	
+
+	mediaTypes: defineCollection({ 
+		loader: glob({ pattern: ['library-kinds.json'], base: "./src/data" }),
+		schema: z.array(
+			z.object({
+				title: z.string(),
+			})
+		),
+	}),
+
 	bookmarks: defineCollection({ 
 		loader: glob({ pattern: ['bookmark-kinds.json'], base: "./src/data" }),
 		schema: z.array(
