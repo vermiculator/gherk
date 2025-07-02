9890fc1fe3f9605568646d19566ba721cc3f79e6
vermiculator <rowanlucas@pm.me>
2025-07-02 20:22:05 +0100

    not broken

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,87 +5,100 @@ import react from '@astrojs/react';
 import starlightObsidian, { obsidianSidebarGroup } from 'starlight-obsidian';
 import starlightSiteGraph from 'starlight-site-graph';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [starlight({
-        title: 'gherk',
-        customCss: [
-            './src/styles/custom.css',
-        ],
-        social: [
-            { icon: 'github', label: 'GitHub', href: 'https://github.com/rowanlucas' },
-        ],
-        sidebar: [
-            {
-                label: 'earth',
-                collapsed: true,
-                autogenerate: { directory: 'earth' },
-            },
-            {
-                label: 'library',
-                collapsed: true,
-                autogenerate: { directory: 'library' },
-            },
-            {
-                label: 'vignettes',
-                link: '/vignettes'
-            },
-        ],
-        plugins: [
-            starlightObsidian({
-              vault: './src/content/vault',
-              ignore: ['./*.md', 'id.md','.filenignore','.megaignore','*.acsm','LICENSE','*.txt','*.pdf','desktop.ini','./Rubbish','_.debris','.trash','./seeds', './private', './assets', './themes', './vignettes'],
-              output: '',
-              autoLinkHeadings: true,
-              copyFrontmatter: 'all',
-              tableOfContentsOverview: 'title'
-            }),
-            starlightSiteGraph({
-                debug: false,
-                trackVisitedPages: 'disable',
-                sitemapConfig: {
-                    includeExternalLinks: true,
-                    ignoreStarlightLinks: true,
-                    linkInclusionRules: ["!https://read.readwise.io/read/*", "**/*"],
-                },
-                backlinksConfig: {},
-                graphConfig: {
-                    "depth": 2,
-                    "depthDirection": "both",
-                    "actions": [
-                        "fullscreen",
-                        "depth"
+  integrations: [
+  starlight({
+      title: 'gherk',
+      customCss: [
+          './src/styles/custom.css',
+      ],
+      social: [
+          { icon: 'github', label: 'GitHub', href: 'https://github.com/vermiculator' },
+      ],
+     components: {
+      PageSidebar: './src/components/PageSidebar.astro',
+      },
+      sidebar: [
+          {
+              label: 'earth',
+              collapsed: true,
+              autogenerate: { directory: 'earth' },
+          },
+          {
+              label: 'library',
+              collapsed: true,
+              autogenerate: { directory: 'library' },
+          },
+          {
+              label: 'vignettes',
+              link: '/vignettes'
+          },
+          {
+              label: 'mulch',
+              link: '/mulch'
+          },
+      ],
+      plugins: [
+          starlightObsidian({
+            vault: './src/content/vault',
+            ignore: ['./*.md', 'id.md','.filenignore','.megaignore','*.acsm','LICENSE','*.txt','*.pdf','desktop.ini','./Rubbish','_.debris','.trash','./seeds', './private', './assets', './themes', './vignettes', './are.na'],
+            output: '',
+            copyFrontmatter: 'all',
+            tableOfContentsOverview: 'title'
+          }),
+          starlightSiteGraph({
+              debug: false,
+              trackVisitedPages: 'disable',
+              sitemapConfig: {
+                  includeExternalLinks: true,
+                  ignoreStarlightLinks: true,
+                  linkInclusionRules: ["!https://read.readwise.io/read/*", "**/*"],
+              },
+              backlinksConfig: {},
+              graphConfig: {
+                  "depth": 2,
+                  "depthDirection": "both",
+                  "actions": [
+                      "fullscreen",
+                      "depth"
+                  ],
+                  "repelForce": 333,
+                  "colliderPadding": 18,
+                  "alphaDecay": 0.023,
+                  "enableDrag": true,
+                  "enablePan": true,
+                  "enableZoom": true,
+                  "enableHover": true,
+                  "enableClick": "click",
+                  "prefetchPages": false,
+                  "renderUnresolved": true,
+                  "renderArrows": true,
+                  "renderExternal": true,
+                  "nodeDefaultStyle": {
+                      "shape": "circle",
+                      "shapeSize": 4,
+                      "shapeColor": "backgroundColor",
+                      "strokeColor": "nodeColor",
+                      "strokeWidth": 2,
+                      "cornerType": "bevel",
+                      "neighborScale": 0.4,
+                    },
+                  "linkWidth": 2,
+                  "linkHoverWidth": 3,
+                  "visibilityRules": [
+                      "**/*"
                     ],
-                    "repelForce": 333,
-                    "colliderPadding": 18,
-                    "alphaDecay": 0.023,
-                    "enableDrag": true,
-                    "enablePan": true,
-                    "enableZoom": true,
-                    "enableHover": true,
-                    "enableClick": "click",
-                    "prefetchPages": false,
-                    "renderUnresolved": true,
-                    "renderArrows": true,
-                    "renderExternal": true,
-                    "nodeDefaultStyle": {
-                        "shape": "circle",
-                        "shapeSize": 4,
-                        "shapeColor": "backgroundColor",
-                        "strokeColor": "nodeColor",
-                        "strokeWidth": 2,
-                        "cornerType": "bevel",
-                        "neighborScale": 0.4,
-                      },
-                    "linkWidth": 2,
-                    "linkHoverWidth": 3,
-                    "visibilityRules": [
-                        "**/*"
-                      ],
 
-                },
-            }),
-          ]
-          //.concat(process.env.CHECK_LINKS ? starlightLinksValidator() : []),
-		}), react()],
+              },
+          }),
+        ]
+        //.concat(process.env.CHECK_LINKS ? starlightLinksValidator() : []),
+      }), react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
